5b3d2ba5c8b28ee24611c7c3cfb8bb3190d887c0
mehdi78800 <45394380+mehdi78800@users.noreply.github.com>
2018-12-20 19:54:13 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,217 @@
+<?php
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>FCL: France China leaders</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <!-- /* NAVAR DEBUT*/ -->
+
+  <nav class="navbar navbar-expand-lg navbar-dark  ">
+    <a class="navbar-brand" href="#">FCL</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav mx-auto">
+        <a class="nav-item nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="#">PRESENTATION</a>
+        <a class="nav-item nav-link" href="#">PROGRAM</a>
+        <a class="nav-item nav-link" href="#">PARTICIPANTS</a>
+        <a class="nav-item nav-link" href="#">PARTNERS&SPONSORS</a>                   <a class="nav-item nav-link" href="#">CONTACT</a>
+        <a class="nav-item nav-link" href="#"><img src="images/picto-linkedin.png" id="in"></a>
+        <a class="nav-item nav-link" href="#"><img src="images/facebook_.png" id="in"></a>
+        <a class="nav-item nav-link" href="#"><img src="images/_twitter.png" id="in"></a>
+      </div>
+    </div>
+  </nav>
+
+  <!-- /* NAVAR FIN*/ -->
+
+
+  <!-- /* LOGO HAUT + SEARCH DEBUT*/ -->
+  <div class="flex-container">
+    <div> <img src="images/FCL1.png" alt=""> </div>
+    <div> <img src="images/trait-vertical-bleu - long.png" alt=""> </div>
+    <div> <img src="images/logo_club21eme.jpg" alt=""> </div>
+
+
+    <form class="form-inline  ">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="fa fa-search btn btn-primary  " type="submit"></button>
+    </form>
+  </div>
+  <!-- /* LOGO HAUT + SEARCH FIN*/ -->
+
+
+
+  <!-- /* TEXT + 1ST CAROUSEL */ -->
+
+  <div class="container-fluid firstfond">
+    <div class="row">
+      <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+      </div>
+
+      <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8" >
+        <h5> The second France-China Leaders High-Level Meeting for Business Leaders will take place on </h5>
+      </div>
+
+      <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+      </div>
+
+      <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8" >
+        <h1>13th & 14th November 2014 in Bordeaux</h1>
+      </div>
+
+      <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+      </div>
+
+      <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8" >
+        <img id="trait" src="images/trait.png" alt="">
+      </div>
+
+      <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+      </div>
+
+      <div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3" >
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol id="myCarousel-indicators" class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+          </ol>
+          <div class="carousel-inner innerone">
+            <div class="carousel-item active premier">
+              <img src="images/Arnaud.jpeg" alt="First slide">
+              <img src="images/Boulay.jpeg" alt="First slide">
+              <img src="images/tieut.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item premier">
+              <img src="images/CHEN_linag.jpeg" alt="First slide">
+              <img src="images/COPE.jpeg" alt="First slide">
+              <img src="images/christophe.jpeg" alt="First slide">
+            </div>
+            <div class="carousel-item premier">
+              <img src="images/Eric.jpeg" alt="First slide">
+              <img src="images/Hakim_El_Karoui.jpg" alt="First slide">
+              <img src="images/DI.jpeg" alt="First slide">
+            </div>
+            <div class="carousel-item premier">
+              <img src="images/Eukue.jpeg" alt="First slide">
+              <img src="images/ky.jpeg" alt="First slide">
+              <img src="images/François.jpeg" alt="First slide">
+            </div>
+            <div class="carousel-item premier">
+              <img src="images/Lanvin.jpeg" alt="First slide">
+              <img src="images/Luc.jpeg" alt="First slide">
+              <img src="images/Lin.jpeg" alt="First slide">
+            </div>
+            <div class="carousel-item premier">
+              <img src="images/olivier.jpeg" alt="First slide">
+              <img src="images/Mariee.jpeg" alt="First slide">
+              <img src="images/Leon_Laulausa.jpg" alt="First slide">
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5" >
+        <h4> An initiative of Club XXIe Siècle</h4>
+        <p>The France-China Leaders (FCL) Summit is an initiative of Club XXIe Siècle. It aims to provide an exclusive networking platform for business leaders-senior executives and successful entrepreneurs from listed companies in France and China, adress key issues and enhance business cooperation between France and China
+        </p>
+      </div>
+
+      <div class="col-0 col-sm-0 col-md-2 col-lg-2 col-xl-2">
+      </div>
+    </div>
+  </div>
+
+<div class="trait">
+</div>
+
+
+<div class="class="container-fluid"">
+  <div class="row">
+
+    <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+    </div>
+
+    <div class="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10" >
+      <div id="" class="carousel slide" data-ride="carousel">
+
+        <div class="carousel-inner">
+          <div class="carousel-item active deuxieme">
+            <img src="images/tsinghua_sem.png">
+            <img src="images/LOG_KIRWAN.png">
+            <img src="images/ChineInfo_.png">
+            <img src="images/logo-Invest-securities.png">
+            <img src="images/huawei-logo.jpg">
+          </div>
+          <div class="carousel-item deuxieme">
+            <img src="images/logo talan.jpg">
+            <img src="images/logo_club21eme.jpg">
+            <img src="images/ccpit_logo.png">
+            <img src="images/vae.png">
+            <img src="images/bordeau_université.png">
+          </div>
+          <div class="carousel-item deuxieme">
+               <img src="images/institut.png">
+               <img src="images/la_cub.png">
+               <img src="images/eurosearch.jpg">
+               <img src="images/escp.png">
+               <img src="images/bordeaux-logo.jpg">
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+
+
+
+  <!-- /* TEXT + 1ST CAROUSEL FIN*/ -->
+
+
+  <div class="petit">
+  </div>
+
+  <footer>
+    <a class="footlink" href="#">Copyright FCL © 2014</a>
+    <a class="footlink" href="#">Mentions légales</a>
+  </footer>
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
+ ?>
